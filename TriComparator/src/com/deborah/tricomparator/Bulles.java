9d580abaafb0c132104503fdd033156f3f287796
Deborah <Deborah@Deborah-PC>
2018-03-26 21:33:17 +0200

    bulles

---
+++ TriComparator/src/com/deborah/tricomparator/Bulles.java
@@ -0,0 +1,40 @@
+package com.deborah.tricomparator;
+
+public class Bulles {
+	
+private static final Bulles Instance = new Bulles();
+	
+	public static Bulles getInstance() {
+		return Instance;
+	}
+	
+	public static void tri_bulles(int[] list){
+
+	    boolean ordre = false;
+	    int taille = list.length;
+
+	    while(!ordre){
+	        ordre = true;
+	        for(int i=0 ;i<taille-1;i++){
+	            if(list[i] > list[i+1]){
+	                echanger(list,i,i+1);
+	                ordre = false;
+	            }
+	        }
+	        taille--;
+	    }
+
+	}
+	
+	public static void echanger(int[] list,int i,int j) {
+		int temp = list[i];
+		list[i] = list[j];
+		list[j] = temp;
+	}
+	
+		
+}
+
+
+
+
