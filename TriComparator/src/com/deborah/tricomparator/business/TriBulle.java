c184817c9a5abb80f77fa56ab39da41081d9ef5b
absidibe <ali@followanalytics.com>
2018-03-27 13:23:08 +0200

    operation elementaire

--- TriComparator/src/com/deborah/tricomparator/business/TriBulle.java
+++ TriComparator/src/com/deborah/tricomparator/business/TriBulle.java
@@ -7,20 +7,21 @@ import com.deborah.tricomparator.utils.ElementUtils;
 public class TriBulle {
 
 	public int trier(Element [] elements) {
-		boolean isSorted;
+		boolean triee;
 		int comparaison = 0;
 		int affectation = 0;
 		for (int i=0; i<elements.length-1; i++) {
-			isSorted = true;
+			triee = true;
 			for (int j=0; j<elements.length-i-1; j++) {
 				comparaison++;
 				if (elements[j+1].getKey() < elements[j].getKey()) {
+					comparaison++;
 					ElementUtils.echanger(elements, j, j + 1);
 					affectation++;
-					isSorted = false;
+					triee = false;
 				}
 			}
-			if (isSorted)  {
+			if (triee)  {
 				break;
 			}
 		}
